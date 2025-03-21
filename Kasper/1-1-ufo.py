7348a1d0758afe963e2016de8a8279122c3f0cb4
AverageKasper <averagekasper@gmail.com>
2025-03-21 15:34:05 +0200

    add 1-3 graph implementation

--- Kasper/1-1-ufo.py
+++ Kasper/1-1-ufo.py
@@ -9,28 +9,39 @@ i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
-x = 0
-colour = 1
+
 ufo = "<=>"
 position = 50
 speed = 1
+
 while True:
+
     oled.fill(0)
     oled.text(ufo, position, 55)
     oled.show()
+
     if button_1.value() == 0:
+
         position -= speed
+
         if position < -1:
             position = -1
+
     if button_3.value() == 0:
+
         position += speed
+
         if position > 105:
             position = 105
+
     if button_2.value() == 0:
+
         oled.fill(0)
         oled.text("Game Over", 20, 20)
         oled.show()
+
         time.sleep(2)
+
         oled.fill(0)
         oled.show()
         break
--- Kasper/clear-screen.py
+++ Kasper/clear-screen.py
@@ -1,9 +1,6 @@
-import time 
-from machine import UART, Pin, I2C, Timer, ADC 
+from machine import Pin, I2C 
 from ssd1306 import SSD1306_I2C 
-SW0 = Pin(9, Pin.IN, Pin.PULL_UP) 
-SW1 = Pin(8, Pin.IN, Pin.PULL_UP) 
-SW2 = Pin(7, Pin.IN, Pin.PULL_UP) 
+
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_width = 128 
 oled_height = 64 
